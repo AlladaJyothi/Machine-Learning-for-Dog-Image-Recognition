943b6631ce27ae3da3c10e4075a03a4c4e383625
AlladaJyothi <alladajyothi12@gmail.com>
2024-07-24 17:17:19 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
-import os
 import cv2
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 import pickle
 from PIL import Image
