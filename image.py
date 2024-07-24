96c80eef4ad20c95c07574781dcfee10cd7789b0
AlladaJyothi <alladajyothi12@gmail.com>
2024-07-24 17:03:04 +0530

    Update image.py

--- image.py
+++ image.py
@@ -9,11 +9,11 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 from PIL import Image
 
-st.image(r"C:\Users\DELL\Pictures\inno_image.webp")
+st.image("inno_image.webp")
 name=st.title(r'Machine Learning for Dog Image Recognition')
 
 
-model = pickle.load(open(r'C:\\Users\\DELL\\Machine Learning\\image.pkl','rb'))
+model = pickle.load(open("image.pkl','rb'))
 
 image=st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 st.image(image,caption='Uploaded Image',use_column_width=True)
